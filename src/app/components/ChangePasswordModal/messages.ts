27d2f3ea05ade4a4cca03e0b93354409d3d68ab6
Phuong Nghiem <phuong.nghiem@hdwebsoft.co>
2021-05-05 17:39:09 +0700

    fix message ChangePasswordModal & DeleteConfirmModal

--- src/app/components/ChangePasswordModal/messages.ts
+++ src/app/components/ChangePasswordModal/messages.ts
@@ -8,21 +8,45 @@ import { translations } from 'locales/translations';
 import { _t } from 'utils/messages';
 
 export const messages = {
-  changePasswordTitle: () => _t(translations.changPassword.title),
-  changePasswordCancel: () => _t(translations.changPassword.cancel),
-  changePasswordOld: () => _t(translations.changPassword.oldPassword),
-  changePasswordNew: () => _t(translations.changPassword.newPassword),
-  changePasswordRetype: () => _t(translations.changPassword.retypePassword),
-  changePasswordIsRequired: () => _t(translations.changPassword.isRequired),
-  changePasswordMinLength: () => _t(translations.changPassword.minLength),
+  changePasswordTitle: () =>
+    _t(translations.changPassword.title, 'Change Password'),
+  changePasswordCancel: () => _t(translations.changPassword.cancel, 'Cancel'),
+  changePasswordOld: () =>
+    _t(translations.changPassword.oldPassword, 'Old Password'),
+  changePasswordNew: () =>
+    _t(translations.changPassword.newPassword, 'New Password'),
+  changePasswordRetype: () =>
+    _t(translations.changPassword.retypePassword, 'Confirm Password'),
+  changePasswordIsRequired: () =>
+    _t(translations.changPassword.isRequired, '(*) This field is required'),
+  changePasswordMinLength: () =>
+    _t(translations.changPassword.minLength, '(*) Min length 8'),
   changePasswordComplexPassword: () =>
-    _t(translations.changPassword.complexPassword),
-  changePasswordIsNotMatch: () => _t(translations.changPassword.isNotMatch),
-  changePasswordSuccess: () => _t(translations.changPassword.changePassSuccess),
-  changePasswordFailed: () => _t(translations.changPassword.changePassFailed),
-  changePasswordWrongOld: () => _t(translations.changPassword.wrongOldPassword),
-  changePasswordInputOld: () => _t(translations.changPassword.inputOldPassword),
-  changePasswordInputNew: () => _t(translations.changPassword.inputNewPassword),
+    _t(translations.changPassword.complexPassword, 'complexPassword'),
+  changePasswordIsNotMatch: () =>
+    _t(
+      translations.changPassword.isNotMatch,
+      '(*) The two passwords that you entered do not match!',
+    ),
+  changePasswordSuccess: () =>
+    _t(translations.changPassword.changePassSuccess, 'Change password success'),
+  changePasswordFailed: () =>
+    _t(translations.changPassword.changePassFailed, 'Change password failed'),
+  changePasswordWrongOld: () =>
+    _t(translations.changPassword.wrongOldPassword, 'Wrong Old Password'),
+  changePasswordInputOld: () =>
+    _t(
+      translations.changPassword.inputOldPassword,
+      'Please input old password!',
+    ),
+  changePasswordInputNew: () =>
+    _t(
+      translations.changPassword.inputNewPassword,
+      'Please input new password!',
+    ),
   changePasswordInputRetype: () =>
-    _t(translations.changPassword.inputRetypePassword),
+    _t(
+      translations.changPassword.inputRetypePassword,
+      'Please input retype password!',
+    ),
 };
