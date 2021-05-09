5e2d0857578f6e26e08a2b0ebb90943374ee69c1
Phat Nguyen <hoaiphat0206@gmail.com>
2021-05-09 22:40:12 +0700

    Refactor Any Type

--- src/app/pages/ForgotPassword/useSendMailForgot.ts
+++ src/app/pages/ForgotPassword/useSendMailForgot.ts
@@ -1,7 +1,7 @@
 import { ToastMessageType, useNotify } from 'app/components/ToastNotification';
 import React from 'react';
 import { useHistory } from 'react-router';
-import fakeAPI from 'utils/fakeAPI';
+import { api } from 'utils/api';
 
 interface ForgotPasswordPayload {
   email: string;
@@ -21,16 +21,13 @@ export const useSendMailForgot = (): ForgotPasswordHook => {
   const send = async (data: ForgotPasswordPayload): Promise<void> => {
     try {
       setLoading(true);
-      await fakeAPI
-        .post('/auth/password/reset/', { email: data.email })
-        .then((response: any) => {
-          notify({
-            type: ToastMessageType.Info,
-            message: response.detail,
-            duration: 2,
-          });
-          history.push('/reset-password');
-        });
+      await api.auth.resetPassword(data.email);
+      notify({
+        type: ToastMessageType.Info,
+        message: 'Email has been sent',
+        duration: 2,
+      });
+      history.push('/reset-password');
     } catch (e) {
       setError(e);
       notify({
