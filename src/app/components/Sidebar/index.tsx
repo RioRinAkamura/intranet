79bb7140b9b78f0d2d36444ed9dcd9abcd1f4ec9
khanh <khanhnqps10920@fpt.edu.vn>
2021-06-17 18:42:52 +0700

    fix/clean-up-menu: fix build eslint

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 import {
-  AppstoreOutlined,
   PieChartOutlined,
   UserOutlined,
   TeamOutlined,
@@ -18,7 +17,6 @@ interface Props {
 
 const SideBar: React.FC<Props> = ({ collapsed, onCollapse }) => {
   const { Sider } = Layout;
-  const { SubMenu } = Menu;
   const [isSidebar] = React.useState(isMobile);
   return (
     <Wrapper style={{ width: collapsed ? 80 : 200 }}>
