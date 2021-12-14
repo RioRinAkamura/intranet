d2ab9bac5be4ef76455480c040cffaee637d28b9
manhtran1611 <85244387+manhtran1611@users.noreply.github.com>
2021-12-14 14:31:19 +0700

    Added clean up data before format

--- src/utils/phoneFormat.ts
+++ src/utils/phoneFormat.ts
@@ -1,8 +1,10 @@
 export function phoneFormat(phone: string): string {
-  if (phone.length === 10) {
-    return phone.replace(/(\d{3})(\d{3})(\d{4})/, `$1 $2 $3`);
-  } else if (phone.length === 11) {
-    return phone.replace(/(\d{4})(\d{3})(\d{4})/, `$1 $2 $3`);
+  let cleanedPhone = ('' + phone).replace(/\D/g, '');
+
+  if (cleanedPhone.length === 10) {
+    return cleanedPhone.replace(/(\d{3})(\d{3})(\d{4})/, `$1 $2 $3`);
+  } else if (cleanedPhone.length === 11) {
+    return cleanedPhone.replace(/(\d{4})(\d{3})(\d{4})/, `$1 $2 $3`);
   } else {
     // This is temporary, if there are other phone format, add more
     return phone;
